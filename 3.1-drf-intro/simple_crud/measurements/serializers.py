566e3c3c77fb92f38b3ba214ed7c7a24460e2912
Sergey <earnedgravy@gmail.com>
2021-12-12 13:44:11 +0700

    task implemented

--- 3.1-drf-intro/simple_crud/measurements/serializers.py
+++ 3.1-drf-intro/simple_crud/measurements/serializers.py
@@ -1,1 +1,16 @@
-# TODO: опишите сериализаторы
+from rest_framework import serializers
+from measurements.models import Project, Measurement
+
+
+class ProjectSerialiser(serializers.ModelSerializer):
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class MeasurementSerialiser(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measurement
+        fields = '__all__'
--- 3.1-drf-intro/simple_crud/simple_crud/urls.py
+++ 3.1-drf-intro/simple_crud/simple_crud/urls.py
@@ -1,5 +1,4 @@
 """simple_crud URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
@@ -14,10 +13,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from measurements.views import ProjectViewSet, MeasurementViewSet
 
-# TODO: настройте роутер и подключите `ProjectViewSet` и `MeasurementViewSet`
+
+router = routers.DefaultRouter()
+router.register('project', ProjectViewSet, basename='project')
+router.register('measurement', MeasurementViewSet, basename='measurement')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
 ]
